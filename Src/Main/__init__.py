5777c746ae04009999d2a0d7a85587641bb02b9c
diogofernandesc <diogofernandescon@gmail.com>
2015-03-08 23:21:33 +0000

    Game is essentially complete fully working will add extra comments

--- Src/Main/__init__.py
+++ Src/Main/__init__.py
@@ -1,9 +1,7 @@
 '''Include:
-- Crash function, what happens without colour?
-- Counter for resets
-- Countdown
-- Tidy up functions
 
+**********************
+- Do I allow player to retry from current level or start all over again???
 '''
 # Defining colours for the circles, paths(lines) and grid background
 
@@ -195,17 +193,6 @@ YellowLink = False
 GreenLink = False
 BlueLink = False
 
-'''def message_display(text):
-    largeText = pygame.font.Font('freensansbold.ttf', 115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    screen.blit(TextSurf, TextRect)
-    pygame.display.update()
-    time.sleep(2)'''
-
-
-def crash():
-    message_display("You Crashed")
 
 def text(text, text_x, text_y, size):
     TextFont = pygame.font.Font('freesansbold.ttf', size)
@@ -231,8 +218,8 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
     if button_x+button_w > mouse_x > button_x and button_y+button_h > mouse_y > button_y:
         pygame.draw.rect(screen, acolour, (button_x,button_y,button_w,button_h))
         if click[0] == 1 and action!= None:
-            if action == "play":
-                game_level(1, "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
+            if action == "level 1":
+                game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
             
             elif action == "instructions":
                 instruction_menu()
@@ -251,7 +238,7 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     screen.blit(reset_text,(5,625))
                 else:
                     line_colour = ()
-                    game_level(1, "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
+                    game_level("level 1", "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
 
                     
             elif action == "reset_level2":
@@ -261,7 +248,7 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     screen.blit(reset_text,(5,625))
                 else:
                     line_colour = ()
-                    game_level(2, "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
+                    game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
             
             elif action == "reset_level3":
                 if nresets == -1:
@@ -270,7 +257,7 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     screen.blit(reset_text,(5,625))
                 else:
                     line_colour = ()
-                    game_level(3, "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
+                    game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
                     
             elif action == "reset_level4":
                 if nresets == -1:
@@ -279,7 +266,7 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     screen.blit(reset_text,(5,625))
                 else:
                     line_colour = ()
-                    game_level(4, "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
+                    game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
 
             elif action == "reset_level5":
                 if nresets == -1:
@@ -288,20 +275,20 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
                     screen.blit(reset_text,(5,625))
                 else:
                     line_colour = ()
-                    game_level(5, "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
+                    game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
 
                 
             elif action == "level 2":
-                game_level(2, "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
+                game_level("level 2", "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
 
             elif action == "level 3":
-                game_level(3, "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
+                game_level("level 3", "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
     
             elif action == "level 4":
-                game_level(4, "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
+                game_level("level 4", "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
             
             elif action == "level 5":
-                game_level(5, "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
+                game_level("level 5", "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
 
             elif action == "game complete":
                 game_finished()
@@ -318,31 +305,83 @@ def button(msg, button_x, button_y, button_w, button_h, icolour, acolour, fontsi
     textRect.center = ((button_x+(button_w/2),(button_y+(100/2))))
     screen.blit(textSurf, textRect)
 
+
+
+def level_finished(situation, nextlevel, current_level):
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True  # Closes the game and exits the loop
+        
+        button("",20,170, 560, 150, Lightblue, Lightblue, 30, "none")
+        button("",20,380, 560, 150, Lightblue, Lightblue, 30, "none")
+        
+        if situation == "level complete":
+            # This will determine what will happen when the level is completed:
+            # At this point a message comes up saying level complete after all have been linked
+            text = font.render("Level Complete", True, Black)
+            textpos = text.get_rect(centerx=background.get_width()/2)
+            textpos.top = 210
+            screen.blit(text, textpos)
+            button("Next level",50,400, 200, 100, DarkGreen, Green, 30, nextlevel)
+            button("Quit game",350,400, 200, 100, DarkGrey, Grey, 30, "quit")
+            
+        elif situation == "out of time":
+            text = font.render("Out of time!", True, Black)
+            textpos = text.get_rect(centerx=background.get_width()/2)
+            textpos.top = 210
+            screen.blit(text, textpos)
+            button("Retry",50,400, 200, 100, DarkGreen, Green, 30, current_level)
+            button("Quit game",350,400, 200, 100, DarkGrey, Grey, 30, "quit")
+            
+            
+        elif situation == "game complete":
+            text = font.render("Game Complete!", True, Black)
+            textpos = text.get_rect(centerx=background.get_width()/2)
+            textpos.top = 210
+            screen.blit(text, textpos)
+            button("Main Menu",200,400, 200, 100, DarkGreen, Green, 30, "main menu")
+            
+        
+        pygame.display.flip()
+        
+            
+        
+
 def level_complete(nextlevel):
-    # Need to clean this up to one function:
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True  # Closes the game and exits the loop
+                
+                
+    
+        # Need to clean this up to one function:
+        button("",20,170, 560, 150, Lightblue, Lightblue, 30, "none")
+        button("",20,380, 560, 150, Lightblue, Lightblue, 30, "none")
+        # This will determine what will happen when the level is completed:
+        # At this point a message comes up saying level complete after all have been linked
+        text = font.render("Level Complete", True, Black)
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        textpos.top = 210
+        screen.blit(text, textpos)
+        button("Next level",50,400, 200, 100, DarkGreen, Green, 30, nextlevel)
+        button("Quit game",350,400, 200, 100, DarkGrey, Grey, 30, "quit")
+                
+        pygame.display.flip()
+        
+
+
+def retry(current_level):
     button("",20,170, 560, 150, Lightblue, Lightblue, 30, "none")
     button("",20,380, 560, 150, Lightblue, Lightblue, 30, "none")
-    # This will determine what will happen when the level is completed:
-    # At this point a message comes up saying level complete after all have been linked
-    text = font.render("Level Complete", True, Black)
-    textpos = text.get_rect(centerx=background.get_width()/2)
-    textpos.top = 210
-    screen.blit(text, textpos)
-    button("Next level",50,400, 200, 100, DarkGreen, Green, 30, nextlevel)
-    button("Quit game",350,400, 200, 100, DarkGrey, Grey, 30, "quit")
-
-
-
-def retry():
-    button("",20,170, 560, 150, Lightblue, Lightblue, 30, "none")
-    button("",20,380, 560, 150, Lightblue, Lightblue, 30, "none")
-    # This will determine what will happen when the level is completed:
-    # At this point a message comes up saying level complete after all have been linked
     text = font.render("Out of time!", True, Black)
     textpos = text.get_rect(centerx=background.get_width()/2)
     textpos.top = 210
     screen.blit(text, textpos)
-    button("Retry",50,400, 200, 100, DarkGreen, Green, 30, "play")
+    button("Retry",50,400, 200, 100, DarkGreen, Green, 30, current_level)
     button("Quit game",350,400, 200, 100, DarkGrey, Grey, 30, "quit")
 
 def game_finished():
@@ -357,7 +396,7 @@ def game_finished():
     button("Main Menu",375,400, 200, 100, DarkGreen, Green, 30, "main menu")
 
 
-def timer():
+def timer(situation, nextlevel, current_level):
     global frame_count
     global frame_rate
     global level_time
@@ -378,11 +417,11 @@ def timer():
     if total_seconds < 0:
         # Statements executed when time runs out:
         total_seconds = 0
-        retry()
+        level_finished(situation, nextlevel, current_level)
         
     else:
         # Need this to create a black background over the blitted text as to not overlap
-        button("", 210, 605, 125, 20, Black, Black, 10, "none") 
+        button("", 210, 605, 135, 20, Black, Black, 10, "none") 
         # Divide by 60 to get total minutes
         minutes = total_seconds // 60
          
@@ -432,7 +471,7 @@ def instruction_menu():
         text("4. You can only reset the level a maximum of 3 times",300, 450, 15)
         text("5. Enjoy yourself!", 300,500, 15)
         
-        button("Play", 50,600,200,100, DarkGreen, Green,30, "play")
+        button("Play", 50,600,200,100, DarkGreen, Green,30, "level 1")
         button("Quit",350,600,200,100, DarkRed, Red,30, "quit")
         
         pygame.display.update()
@@ -453,7 +492,7 @@ def game_intro():
         TextRect.center = ((display_width/2),(display_height*0.25))
         screen.blit(TextSurf, TextRect)
         
-        button("Play", 200,300,200,100, DarkGreen, Green,30, "play")
+        button("Play", 200,300,200,100, DarkGreen, Green,30, "level 1")
         button("Instructions",200,450,200,100, DarkBlue, Blue, 30, "instructions")
         button("Quit",200,600,200,100, DarkRed, Red, 30, "quit")
         
@@ -463,7 +502,7 @@ def game_intro():
 # Arguments: Big letters (R, B, G, Y, O) represent stationary circles, small letters (o, r, g, y, b) used to check connections complete or if clicked
 # Arguments: action to say what happens after level complete, and rAction for action for the reset button
 
-def game_level(game_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x, B2y, G1x, G1y, G2x, G2y, O1x, O1y, O2x, O2y, Y1x, Y1y, Y2x, Y2y, o_x1, o_y1, o_x2, o_y2, r_x1, r_y1, r_x2, r_y2, yel_x1, yel_y1, yel_x2, yel_y2, g_x1, g_y1, g_x2, g_y2, b_x1, b_y1, b_x2, b_y2, orangegp1, orangegp2, redgp1, redgp2, yellowgp1, yellowgp2, greengp1, greengp2, bluegp1, bluegp2):
+def game_level(current_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x, B2y, G1x, G1y, G2x, G2y, O1x, O1y, O2x, O2y, Y1x, Y1y, Y2x, Y2y, o_x1, o_y1, o_x2, o_y2, r_x1, r_y1, r_x2, r_y2, yel_x1, yel_y1, yel_x2, yel_y2, g_x1, g_y1, g_x2, g_y2, b_x1, b_y1, b_x2, b_y2, orangegp1, orangegp2, redgp1, redgp2, yellowgp1, yellowgp2, greengp1, greengp2, bluegp1, bluegp2):
     global Orange1Clicked
     global Orange2Clicked 
     global Red1Clicked
@@ -487,11 +526,10 @@ def game_level(game_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x, B
     global frame_rate
     global level_time
     # Naming the caption of the window opened for the Game
-    pygame.display.set_caption("Flow level "+ str(game_level))
+    pygame.display.set_caption("Flow " + current_level)
     
     # Set which level is currently being played:
     
-    game_level = 1
     frame_count = 0
     frame_rate = 60
 
@@ -617,30 +655,25 @@ def game_level(game_level, action, rAction, R1x, R1y, R2x, R2y, B1x, B1y, B2x, B
                     Connection.isConnected(orangegp1, orangegp2, redgp1, redgp2, yellowgp1, yellowgp2, greengp1, greengp2, bluegp1, bluegp2)
                             
             if ((OrangeLink == True) and (RedLink == True) and (YellowLink == True) and (GreenLink == True) and (BlueLink == True)):
-                if game_level == 5:
-                    game_finished()
+                time.sleep(1)
+                if current_level == "level 5":
+                    level_finished("game complete", action, current_level)
                     
                 else:
-                    level_complete(action)
+                    level_finished("level complete", action, current_level)
+                    
+                    
                 
                 
          
         # Countdown timer: 
-        timer()
-        button("Reset Paths", 25,650,150,100, DarkBlue, Blue, 20, rAction)                  
-        button("Main menu", 225,650,150,100, DarkGreen, Green, 20, "main menu")
-        button("Quit game", 425,650,150,100, DarkRed, Red, 20, "quit")                       
+        timer("out of time", action, current_level)
+        button("Reset Paths", 25,640,150,100, DarkBlue, Blue, 20, rAction)                  
+        button("Main menu", 225,640,150,100, DarkGreen, Green, 20, "main menu")
+        button("Quit game", 425,640,150,100, DarkRed, Red, 20, "quit")                       
         # Update screen with changes
         pygame.display.flip()
 
-
-'''# Passing arguments for each level:
-game_level(1, "level 2", "reset_level1", 2, 2, 4, 4, 5, 4, 3, 5, 3, 3, 1, 4, 5, 0, 3, 4, 1, 2, 5, 3, 5, 0, 3, 4, 2, 2, 4, 4, 1, 2, 5, 3, 3, 3, 1, 4, 5, 4, 3, 5, 6, 28, 15, 29, 14, 24, 22, 26, 30, 34)
-game_level(2, "level 3", "reset_level2", 0, 1, 5, 3, 1, 1, 5, 4, 3, 1, 4, 4, 1, 3, 0, 5, 1, 2, 1, 4, 1, 3, 0, 5, 0, 1, 5, 3, 1, 2, 1, 4, 3, 1, 4, 4, 1, 1, 5, 4, 20, 31, 7, 24, 14, 26, 10, 29, 8, 30)
-game_level(3, "level 4", "reset_level3", 4, 1, 4, 4, 4, 2, 4, 5, 5, 1, 3, 5, 0, 0, 1, 4, 0, 1, 2, 5, 0, 0, 1, 4, 4, 1, 4, 4, 0, 1, 1, 4, 5, 1, 3, 5, 4, 2, 4, 5, 1, 26, 11, 29, 7, 33, 12, 34, 17, 35)
-game_level(4, "level 5", "reset_level4", 5, 0, 0, 4, 0, 0, 3, 4, 1, 2, 2, 3, 0, 2, 2, 4, 0, 1, 3, 3, 0, 2, 2, 4, 5, 0, 0, 4, 0, 1, 3, 3, 1, 2, 2, 3, 0, 0, 3, 4, 13, 27, 6, 25, 7, 22, 14, 21, 1, 28)
-game_level(5, "game complete", "reset_level5", 1, 2, 3, 2, 2, 4, 4, 4, 1, 1, 2, 5, 5, 0, 0, 5, 5, 1, 3, 5, 5, 0, 0, 5, 1, 2, 3, 2, 5, 1, 3, 5, 1, 1, 2, 5, 2, 4, 4, 4, 6, 31, 14, 16, 12, 34, 8, 33, 27, 29)
-'''
 
 
 game_intro()
